ffc00895654889ae2570f3f920256a2d996ba1f6
ADMIN <ADMIN@36-LAB01>
2025-02-17 14:45:57 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace bai9_tinhdientichchuvihinhtron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNHAP = new System.Windows.Forms.TextBox();
+            this.rdbCV = new System.Windows.Forms.RadioButton();
+            this.rdbDT = new System.Windows.Forms.RadioButton();
+            this.btnDT = new System.Windows.Forms.Button();
+            this.btnCV = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.btnTINH = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(220, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chu Vi và Diện Tích hình tròn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập bán kính";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbDT);
+            this.groupBox1.Controls.Add(this.rdbCV);
+            this.groupBox1.Location = new System.Drawing.Point(499, 91);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(237, 150);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // txtNHAP
+            // 
+            this.txtNHAP.Location = new System.Drawing.Point(166, 102);
+            this.txtNHAP.Name = "txtNHAP";
+            this.txtNHAP.Size = new System.Drawing.Size(156, 20);
+            this.txtNHAP.TabIndex = 3;
+            // 
+            // rdbCV
+            // 
+            this.rdbCV.AutoSize = true;
+            this.rdbCV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbCV.Location = new System.Drawing.Point(19, 46);
+            this.rdbCV.Name = "rdbCV";
+            this.rdbCV.Size = new System.Drawing.Size(77, 24);
+            this.rdbCV.TabIndex = 0;
+            this.rdbCV.TabStop = true;
+            this.rdbCV.Text = "Chu Vi";
+            this.rdbCV.UseVisualStyleBackColor = true;
+            // 
+            // rdbDT
+            // 
+            this.rdbDT.AutoSize = true;
+            this.rdbDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbDT.Location = new System.Drawing.Point(19, 90);
+            this.rdbDT.Name = "rdbDT";
+            this.rdbDT.Size = new System.Drawing.Size(93, 24);
+            this.rdbDT.TabIndex = 0;
+            this.rdbDT.TabStop = true;
+            this.rdbDT.Text = "Diện Tích";
+            this.rdbDT.UseVisualStyleBackColor = true;
+            // 
+            // btnDT
+            // 
+            this.btnDT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDT.Location = new System.Drawing.Point(43, 220);
+            this.btnDT.Name = "btnDT";
+            this.btnDT.Size = new System.Drawing.Size(141, 38);
+            this.btnDT.TabIndex = 4;
+            this.btnDT.Text = "Diện Tích";
+            this.btnDT.UseVisualStyleBackColor = true;
+            // 
+            // btnCV
+            // 
+            this.btnCV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCV.Location = new System.Drawing.Point(236, 221);
+            this.btnCV.Name = "btnCV";
+            this.btnCV.Size = new System.Drawing.Size(143, 37);
+            this.btnCV.TabIndex = 4;
+            this.btnCV.Text = "Chu Vi";
+            this.btnCV.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kết Quả";
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(166, 154);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(156, 20);
+            this.txtKQ.TabIndex = 3;
+            // 
+            // btnTINH
+            // 
+            this.btnTINH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTINH.Location = new System.Drawing.Point(133, 313);
+            this.btnTINH.Name = "btnTINH";
+            this.btnTINH.Size = new System.Drawing.Size(143, 37);
+            this.btnTINH.TabIndex = 4;
+            this.btnTINH.Text = "TÍNH";
+            this.btnTINH.UseVisualStyleBackColor = true;
+         
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTINH);
+            this.Controls.Add(this.btnCV);
+            this.Controls.Add(this.btnDT);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtNHAP);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+           
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNHAP;
+        private System.Windows.Forms.RadioButton rdbCV;
+        private System.Windows.Forms.RadioButton rdbDT;
+        private System.Windows.Forms.Button btnDT;
+        private System.Windows.Forms.Button btnCV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button btnTINH;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai9_tinhdientichchuvihinhtron
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            double bk, chuvi, dientich;
+
+            bk = Convert.ToDouble(txtNHAP.Text);
+            if (rdbCV.Checked)
+            {
+                chuvi = 2 * Math.PI * bk;
+                txtKQ.Text = chuvi.ToString();
+            }
+            if (rdbDT.Checked)
+            {
+                dientich = (Math.PI * (bk * bk));
+                txtKQ.Text = dientich.ToString();
+            }
+        }
+    }
+}
